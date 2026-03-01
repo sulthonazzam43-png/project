92c096e395038edbf1fe60ded52ec2b486e85792
sulthonazzam43-png <sulthonazzam43@gmail.com>
2026-03-01 15:43:08 +0700

    Update deploy.js

--- api/deploy.js
+++ api/deploy.js
@@ -7,24 +7,27 @@ return res.status(405).json({ error: "Method not allowed" });
 try {
 
 const VERCEL_TOKEN = process.env.VERCEL_TOKEN;
-
 const { html, name } = req.body;
+
+if (!VERCEL_TOKEN) {
+return res.json({ error: "VERCEL_TOKEN belum di set" });
+}
 
 if (!html || !name) {
 return res.json({ error: "Data tidak lengkap" });
 }
 
-/*
-Deploy ke Vercel API
-*/
 const response = await fetch("https://api.vercel.com/v13/deployments", {
 method: "POST",
 headers: {
-"Authorization": `Bearer ${VERCEL_TOKEN}`,
+Authorization: `Bearer ${VERCEL_TOKEN}`,
 "Content-Type": "application/json"
 },
 body: JSON.stringify({
 name: name.toLowerCase(),
+projectSettings: {
+framework: null
+},
 files: [
 {
 file: "index.html",
@@ -36,15 +39,16 @@ data: html
 
 const data = await response.json();
 
-if (data.url) {
+/* tampilkan error asli */
+if (!data.url) {
 return res.json({
-ok: true,
-url: "https://" + data.url
+error: data
 });
 }
 
 return res.json({
-error: data
+ok: true,
+url: "https://" + data.url
 });
 
 } catch (err) {
